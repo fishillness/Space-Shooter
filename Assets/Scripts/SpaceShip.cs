9567402694a0a50b6298c92f5928713a251c2927
Mariya Kolodizhanskaya <fishillness@gmail.com>
2023-02-13 14:12:42 +0600

    Scripts "PowerupStats" and "PowerupWeapon" added. Prefabs "AddRocketWeapon" and "ProjectileRocket" added.

--- Assets/Scripts/SpaceShip.cs
+++ Assets/Scripts/SpaceShip.cs
@@ -186,6 +186,15 @@ namespace SpaceShooter
 
             return false;
         }
+
+        public void AssignWeapon(TurretProperties props)
+        {
+            for (int i = 0; i < m_Turrets.Length; i++)
+            {
+                m_Turrets[i].AssignLoadout(props);
+            }
+        }
+
         #endregion
     }
 
