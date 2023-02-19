1ff4057b572ae7726b01f358408ca4b5f1a00554
Mariya Kolodizhanskaya <fishillness@gmail.com>
2023-02-19 12:25:35 +0600

    Script "PlayerShipSelectionController" added. Script "SpaceShip" changed.

---
+++ Assets/Scripts/Level/PlayerShipSelectionController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpaceShooter
+{
+
+    public class PlayerShipSelectionController : MonoBehaviour
+    {
+        [SerializeField] private SpaceShip m_Prefab;
+
+        [SerializeField] private Text m_Shipname;
+        [SerializeField] private Text m_Hitpoints;
+        [SerializeField] private Text m_Speed;
+        [SerializeField] private Text m_Agility;
+
+        [SerializeField] private Image m_Preview;
+
+        private void Start()
+        {
+            if (m_Prefab != null)
+            {
+                m_Shipname.text = m_Prefab.Nickname;
+                m_Hitpoints.text = "HP : " + m_Prefab.HitPoints.ToString();
+                m_Speed.text = "Speed : " + m_Prefab.MaxLinearVelocity.ToString();
+                m_Agility.text = "Agility : " + m_Prefab.MaxAngularVelocity.ToString();
+                m_Preview.sprite = m_Prefab.PreviewImage;
+            }
+        }
+
+        public void OnButtonSelectShip()
+        {
+            LevelSequenceController.PlayerShip = m_Prefab;
+            MainMenuController.Instance.gameObject.SetActive(true);
+        }
+
+
+    }
+}
--- Assets/Scripts/SpaceShip.cs
+++ Assets/Scripts/SpaceShip.cs
@@ -6,6 +6,7 @@ namespace SpaceShooter
     [RequireComponent(typeof(Rigidbody2D))]
     public class SpaceShip : Destructible
     {
+        #region Properties
         /// <summary>
         /// Mass for automatic installation at rigidbody.
         /// Масса для автоматической установки у rigidbody.
@@ -30,19 +31,24 @@ namespace SpaceShooter
         /// Максимальная линейная скорость.
         /// </summary>
         [SerializeField] private float m_MaxLinearVelocity;
+        public float MaxLinearVelocity => m_MaxLinearVelocity;
 
         /// <summary>
         /// Maximum rotational speed. In degrees/sec
         /// Максимальная вращательная скорость. В градусах/сек
         /// </summary>
         [SerializeField] private float m_MaxAngularVelocity;
-
+        public float MaxAngularVelocity => m_MaxAngularVelocity;
 
         /// <summary>
         /// Saved reference to rigidbody.
         /// Сохраненная ссылки на rigidbody.
         /// </summary>
         private Rigidbody2D m_Rigid;
+
+        [SerializeField] private Sprite m_PreviewImage;
+        public Sprite PreviewImage => m_PreviewImage;
+        #endregion
 
         #region Public API
 
